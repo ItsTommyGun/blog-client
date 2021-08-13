a75036d153c32f405748e2bb44e20f7e6d3f5f80
Tommy <thomasfischer1337@gmail.com>
2021-08-13 14:54:53 +0200

    Check for production env

--- src/helpers/environmentUrl.ts
+++ src/helpers/environmentUrl.ts
@@ -1,5 +1,5 @@
 import PORT from "./port"
 
-const ENVIRONMENT_URL = process.env.PRODUCTION ? 'https://dashboard.heroku.com' : `http://localhost:${PORT}`;
+const ENVIRONMENT_URL = process.env.ENVIRONMENT === 'PRODUCTION' ? 'https://tommycodes-blog.herokuapp.com' : `http://localhost:${PORT}`;
 
 export default ENVIRONMENT_URL;
